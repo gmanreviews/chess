076d0bb7bfe50e666c77a98f8a500324a821b78e
Andrew Robinson <andrew.robinson@gmanreviews.com>
2016-02-25 23:42:12 -0500

    some base class updates
    for posts

---
+++ chess/Models/post_category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace chess.Models
+{
+    public class post_category
+    {
+        public int id { get; set; }
+        public string category { get; set; }
+    }
+
+    public class post_category_model
+    {
+        public post_category create_post_category(post_category post_category)
+        {
+            return post_category;
+        }
+    }
+}
