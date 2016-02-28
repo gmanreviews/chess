deb58e805c247c0eb52333d5bff4a9f694a2517e
Andrew Robinson <andrew.robinson@gmanreviews.com>
2016-02-28 18:20:36 -0500

    autcomplete fails
    jquery problem to be looked at

---
+++ chess/Controllers/PostCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using chess.Models;
+
+namespace chess.Controllers
+{
+    public class PostCategoryController : Controller
+    {
+
+        public ActionResult dropdown()
+        {
+            return View(post_category_model.get_post_categories());
+        }
+
+        public JsonResult json_return()
+        {
+            return Json(new { response = post_category_model.get_post_categories() }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult auto_fill()
+        {
+            return View();
+        }
+    }
+}
---
+++ chess/Scripts/my_scripts/category_auto_fill.js
@@ -0,0 +1,18 @@
+﻿/*
+$("#category_auto_fill").autocomplete({
+    source: function(request, response){
+        $.ajax({
+            url: "/postcategory/json_result",
+            dataType: "jsonP",
+            success: function (data){
+                response (data.response)
+            }
+        })
+    }
+})
+*/
+
+var data = ["test", "more test", "no test"];
+$("#category_auto_fill").autocomplete({
+    source: data
+});
